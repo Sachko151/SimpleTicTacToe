aea73501643bdb09d04448da33d2e74587bcb7e0
Sachko151 <sachko151@abv.bg>
2021-07-04 21:44:05 +0300

    Looking forward to find a better wait method and clean up the code

---
+++ Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid1 = new System.Windows.Forms.Panel();
+            this.grid2 = new System.Windows.Forms.Panel();
+            this.grid3 = new System.Windows.Forms.Panel();
+            this.grid6 = new System.Windows.Forms.Panel();
+            this.grid5 = new System.Windows.Forms.Panel();
+            this.grid4 = new System.Windows.Forms.Panel();
+            this.grid9 = new System.Windows.Forms.Panel();
+            this.grid8 = new System.Windows.Forms.Panel();
+            this.grid7 = new System.Windows.Forms.Panel();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPlayerTurn = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnResetScore = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblGreeting = new System.Windows.Forms.Label();
+            this.mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grid1
+            // 
+            this.grid1.BackColor = System.Drawing.Color.White;
+            this.grid1.Location = new System.Drawing.Point(1, -1);
+            this.grid1.Name = "grid1";
+            this.grid1.Size = new System.Drawing.Size(57, 53);
+            this.grid1.TabIndex = 0;
+            this.grid1.Click += new System.EventHandler(this.grid1_Click);
+            // 
+            // grid2
+            // 
+            this.grid2.BackColor = System.Drawing.Color.White;
+            this.grid2.Location = new System.Drawing.Point(1, 58);
+            this.grid2.Name = "grid2";
+            this.grid2.Size = new System.Drawing.Size(57, 53);
+            this.grid2.TabIndex = 1;
+            this.grid2.Click += new System.EventHandler(this.grid2_Click);
+            // 
+            // grid3
+            // 
+            this.grid3.BackColor = System.Drawing.Color.White;
+            this.grid3.Location = new System.Drawing.Point(1, 117);
+            this.grid3.Name = "grid3";
+            this.grid3.Size = new System.Drawing.Size(57, 53);
+            this.grid3.TabIndex = 1;
+            this.grid3.Click += new System.EventHandler(this.grid3_Click);
+            // 
+            // grid6
+            // 
+            this.grid6.BackColor = System.Drawing.Color.White;
+            this.grid6.Location = new System.Drawing.Point(64, 117);
+            this.grid6.Name = "grid6";
+            this.grid6.Size = new System.Drawing.Size(57, 53);
+            this.grid6.TabIndex = 4;
+            this.grid6.Click += new System.EventHandler(this.grid6_Click);
+            // 
+            // grid5
+            // 
+            this.grid5.BackColor = System.Drawing.Color.White;
+            this.grid5.Location = new System.Drawing.Point(64, 58);
+            this.grid5.Name = "grid5";
+            this.grid5.Size = new System.Drawing.Size(57, 53);
+            this.grid5.TabIndex = 3;
+            this.grid5.Click += new System.EventHandler(this.grid5_Click);
+            // 
+            // grid4
+            // 
+            this.grid4.BackColor = System.Drawing.Color.White;
+            this.grid4.Location = new System.Drawing.Point(64, -1);
+            this.grid4.Name = "grid4";
+            this.grid4.Size = new System.Drawing.Size(57, 53);
+            this.grid4.TabIndex = 2;
+            this.grid4.Click += new System.EventHandler(this.grid4_Click);
+            // 
+            // grid9
+            // 
+            this.grid9.BackColor = System.Drawing.Color.White;
+            this.grid9.Location = new System.Drawing.Point(127, 117);
+            this.grid9.Name = "grid9";
+            this.grid9.Size = new System.Drawing.Size(57, 53);
+            this.grid9.TabIndex = 4;
+            this.grid9.Click += new System.EventHandler(this.grid9_Click);
+            // 
+            // grid8
+            // 
+            this.grid8.BackColor = System.Drawing.Color.White;
+            this.grid8.Location = new System.Drawing.Point(127, 58);
+            this.grid8.Name = "grid8";
+            this.grid8.Size = new System.Drawing.Size(57, 53);
+            this.grid8.TabIndex = 3;
+            this.grid8.Click += new System.EventHandler(this.grid8_Click);
+            // 
+            // grid7
+            // 
+            this.grid7.BackColor = System.Drawing.Color.White;
+            this.grid7.Location = new System.Drawing.Point(127, -1);
+            this.grid7.Name = "grid7";
+            this.grid7.Size = new System.Drawing.Size(57, 53);
+            this.grid7.TabIndex = 2;
+            this.grid7.Click += new System.EventHandler(this.grid7_Click);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.Black;
+            this.mainPanel.Controls.Add(this.grid6);
+            this.mainPanel.Controls.Add(this.grid9);
+            this.mainPanel.Controls.Add(this.grid1);
+            this.mainPanel.Controls.Add(this.grid2);
+            this.mainPanel.Controls.Add(this.grid8);
+            this.mainPanel.Controls.Add(this.grid4);
+            this.mainPanel.Controls.Add(this.grid7);
+            this.mainPanel.Controls.Add(this.grid5);
+            this.mainPanel.Controls.Add(this.grid3);
+            this.mainPanel.Location = new System.Drawing.Point(46, 40);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(185, 170);
+            this.mainPanel.TabIndex = 5;
+            this.mainPanel.MouseHover += new System.EventHandler(this.mainPanel_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(280, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Current Turn";
+            // 
+            // lblPlayerTurn
+            // 
+            this.lblPlayerTurn.AutoSize = true;
+            this.lblPlayerTurn.Location = new System.Drawing.Point(310, 78);
+            this.lblPlayerTurn.Name = "lblPlayerTurn";
+            this.lblPlayerTurn.Size = new System.Drawing.Size(36, 13);
+            this.lblPlayerTurn.TabIndex = 7;
+            this.lblPlayerTurn.Text = "Player";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(455, 78);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(22, 13);
+            this.lblScore.TabIndex = 9;
+            this.lblScore.Text = "0-0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(455, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Score";
+            // 
+            // btnResetScore
+            // 
+            this.btnResetScore.Location = new System.Drawing.Point(19, 238);
+            this.btnResetScore.Name = "btnResetScore";
+            this.btnResetScore.Size = new System.Drawing.Size(177, 45);
+            this.btnResetScore.TabIndex = 10;
+            this.btnResetScore.Text = "Reset The Game";
+            this.btnResetScore.UseVisualStyleBackColor = true;
+            this.btnResetScore.Click += new System.EventHandler(this.btnResetScore_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(229, 238);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(177, 45);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Player";
+            // 
+            // lblGreeting
+            // 
+            this.lblGreeting.AutoSize = true;
+            this.lblGreeting.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblGreeting.ForeColor = System.Drawing.Color.LimeGreen;
+            this.lblGreeting.Location = new System.Drawing.Point(264, 134);
+            this.lblGreeting.Name = "lblGreeting";
+            this.lblGreeting.Size = new System.Drawing.Size(0, 30);
+            this.lblGreeting.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 294);
+            this.Controls.Add(this.lblGreeting);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnResetScore);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblPlayerTurn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mainPanel);
+            this.Name = "Form1";
+            this.Text = "TicTacToe";
+            this.mainPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel grid1;
+        private System.Windows.Forms.Panel grid2;
+        private System.Windows.Forms.Panel grid3;
+        private System.Windows.Forms.Panel grid6;
+        private System.Windows.Forms.Panel grid5;
+        private System.Windows.Forms.Panel grid4;
+        private System.Windows.Forms.Panel grid9;
+        private System.Windows.Forms.Panel grid8;
+        private System.Windows.Forms.Panel grid7;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPlayerTurn;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnResetScore;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblGreeting;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Random rand = new Random();
+            lblPlayerTurn.Text = rand.Next(1, 2).ToString();
+
+        }
+        string[,] playerMap = new string[3, 3];
+        Image iconOfPlayerOne = Image.FromFile("..\\..\\src\\x.png");
+        Image iconOfPlayerTwo = Image.FromFile("..\\..\\src\\o.png");
+        int playerOneScore = 0, playerTwoScore = 0;
+        private void btnResetScore_Click(object sender, EventArgs e)
+        {
+            playerOneScore = playerTwoScore = 0;
+            lblScore.Text = "0-0";
+            ResetGame();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void grid1_Click(object sender, EventArgs e)
+        {
+            gridAction(grid1, 0, 0);
+        }
+
+        private void grid2_Click(object sender, EventArgs e)
+        {
+            gridAction(grid2, 1, 0);
+
+        }
+
+        private void grid3_Click(object sender, EventArgs e)
+        {
+            gridAction(grid3, 2, 0);
+        }
+
+        private void grid4_Click(object sender, EventArgs e)
+        {
+            gridAction(grid4, 0, 1);
+        }
+
+        private void grid5_Click(object sender, EventArgs e)
+        {
+            gridAction(grid5, 1, 1);
+        }
+
+        private void grid6_Click(object sender, EventArgs e)
+        {
+            gridAction(grid6, 2, 1);
+        }
+
+        private void grid7_Click(object sender, EventArgs e)
+        {
+            gridAction(grid7, 0, 2);
+        }
+
+        private void grid8_Click(object sender, EventArgs e)
+        {
+            gridAction(grid8, 1, 2);
+        }
+
+        private void grid9_Click(object sender, EventArgs e)
+        {
+            gridAction(grid9, 2, 2);
+        }
+        private void SwitchTurns()
+        {
+            switch (lblPlayerTurn.Text)
+            {
+                case "1":
+                    lblPlayerTurn.Text = "2";
+                    break;
+                case "2":
+                    lblPlayerTurn.Text = "1";
+                    break;
+                default:
+                    Console.WriteLine("Wtf");
+                    break;
+            }
+        }
+        private bool IsPlayerOneTurn()
+        {
+            return lblPlayerTurn.Text == "1";
+        }
+        private bool CheckIfThePlayerHasWon(int x, int y, string[,] playerMap, string playerCharacter)
+        {
+            string temp = "", temp2 = "";
+            string winCondition = playerCharacter + playerCharacter + playerCharacter;
+            if (x + y == 0 || x + y == 2 || x + y == 4)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    temp += playerMap[i, i];
+                }
+                for (int i = 0, k = 2; k >= 0; i++, k--)
+                {
+                    temp2 += playerMap[i, k];
+                }
+                if (temp == winCondition || temp2 == winCondition) return true;
+            }
+            temp = temp2 = "";
+            for (int i = 0; i < 3; i++)
+            {
+                temp += playerMap[x, i];
+                temp2 += playerMap[i, y];
+            }
+            if (temp == winCondition || temp2 == winCondition) return true;
+            return false;
+        }
+        private void ThePlayerHasWon(int playerId)
+        {
+            string score = lblScore.Text;
+            switch (playerId)
+            {
+                case 1:
+                    lblGreeting.Text = "Congrats to Player One ,\n He Won!";
+                    mainPanel.Enabled = false;
+                    break;
+                default:
+                    lblGreeting.Text = "Congrats to Player Two ,\n He Won!";
+                    mainPanel.Enabled = false;
+                    break;
+            }
+            wait(3000);
+            ResetGame();
+
+        }
+        private void gridAction(Panel currentGrid, int x, int y)
+        {
+
+
+            if (IsPlayerOneTurn())
+            {
+                playerMap[x, y] = "X";
+                currentGrid.BackgroundImage = iconOfPlayerOne;
+                currentGrid.Enabled = false;
+                if (CheckIfThePlayerHasWon(x, y, playerMap, "X"))
+                {
+                    updateScore(playerOneScore++, playerTwoScore);
+                    ThePlayerHasWon(1);
+                    
+                }
+                SwitchTurns();
+            }
+            else
+            {
+                playerMap[x, y] = "O";
+                currentGrid.BackgroundImage = iconOfPlayerTwo;
+                currentGrid.Enabled = false;
+                if (CheckIfThePlayerHasWon(x, y, playerMap, "O"))
+                {
+                    updateScore(playerOneScore, playerTwoScore++);
+                    ThePlayerHasWon(2);
+                    
+                }
+                SwitchTurns();
+            }
+
+        }
+        private bool GameCanStillBePlayed()
+        {
+            return grid1.Enabled || grid2.Enabled || grid3.Enabled ||
+                grid4.Enabled || grid5.Enabled || grid6.Enabled ||
+                grid7.Enabled || grid8.Enabled || grid9.Enabled;
+        }
+        private async void Draw()
+        {
+            lblGreeting.ForeColor = Color.Red;
+            lblGreeting.Text = "Draw!";
+            wait(3000);
+            ResetGame();
+
+        }
+        private void ResetGame()
+        {
+            grid1.BackgroundImage = null;
+            grid2.BackgroundImage = null;
+            grid3.BackgroundImage = null;
+            grid4.BackgroundImage = null;
+            grid5.BackgroundImage = null;
+            grid6.BackgroundImage = null;
+            grid7.BackgroundImage = null;
+            grid8.BackgroundImage = null;
+            grid9.BackgroundImage = null;
+            mainPanel.Enabled = true;
+            grid1.Enabled = true;
+            grid2.Enabled = true;
+            grid3.Enabled = true;
+            grid4.Enabled = true;
+            grid5.Enabled = true;
+            grid6.Enabled = true;
+            grid7.Enabled = true;
+            grid8.Enabled = true;
+            grid9.Enabled = true;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    playerMap[i, j] = "";
+                }
+            }
+            lblGreeting.Text = "";
+            updateScore(playerOneScore, playerTwoScore);
+        }
+
+        private void updateScore(int playerOneScore, int playerTwoScore)
+        {
+            lblScore.Text = playerOneScore + "-" + playerTwoScore;
+        }
+
+        private void mainPanel_MouseHover(object sender, EventArgs e)
+        {
+            if (!GameCanStillBePlayed()) Draw();
+        }
+
+        private void wait(int mili)
+        {
+            Timer timer = new Timer();
+            if (mili == 0 || mili < 0) return;
+            timer.Interval = mili;
+            timer.Enabled = true;
+            timer.Start();
+            timer.Tick += (s, e) =>
+            {
+                timer.Enabled = false;
+                timer.Stop();
+            };
+            while (timer.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+    }
+
+}
